5fa7434ebb3183a3652949834b6eea18600d5253
Markku <markku.nirkkonen@tuni.fi>
2021-05-10 23:52:49 +0300

    3.1

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+const persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"   
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.use(express.json())
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
 })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
